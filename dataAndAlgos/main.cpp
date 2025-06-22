ca6570da129e71063f1e022642acaddf2da66a4e
cal9t <cal9ten@gmail.com>
2025-06-21 22:55:47 -0400

    adding in recur classes

--- dataAndAlgos/main.cpp
+++ dataAndAlgos/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "recurSection.h"
 
 int main() {
 	std::cout << "Finished intro of data and algos" << std::endl;
+	
+	recurSection testing;
+	testing.testThisConnects();
 	return 0;
 }
---
+++ dataAndAlgos/recurSection.cpp
@@ -0,0 +1,7 @@
+#include "recurSection.h"
+
+void recurSection::testThisConnects() {
+	std::cout << "Yes this works" << std::endl;
+
+	return;
+}
