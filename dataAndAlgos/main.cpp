03afb5a38c5e9dd705efe9cbddb0572d6dcc42f7
cal9t <cal9ten@gmail.com>
2025-07-04 23:51:23 -0400

    clear out for next

--- dataAndAlgos/main.cpp
+++ dataAndAlgos/main.cpp
@@ -10,31 +10,6 @@ const void printVector(const std::vector<int>& vec) {
 }
 
 int main() {
-	std::cout << "Finished intro of data and algos" << std::endl;
-	
-	recurSection testing;
-	testing.recurCount(3);
-
-	std::string input = "Hello";
-	std::cout << "Rev string: " << testing.revString(input) << std::endl;
-
-	int reqInt{ 0 };
-	/*
-	std::cout << "How many of fib sequence? ";
-	std::cin >> reqInt;
-
-	testing.printFib(reqInt);
-
-	std::cout << "How many FizzBuzz? ";
-	std::cin >> reqInt;
-
-	testing.printFizzBuzz(reqInt);
-	*/
-	std::vector<int> vec{ 0, 1, 4, 3, 5, 2 };
-
-	vec = testing.bubbleSort(vec);
-
-	printVector(vec);
 
 	return 0;
 }
