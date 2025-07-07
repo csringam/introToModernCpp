abdf0a546ac5e23b6fa8460a18f17296f4426c81
cal9t <cal9ten@gmail.com>
2025-07-06 22:24:31 -0400

    test account class

--- introToCpp/main.cpp
+++ introToCpp/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "Checking.h"
 
 int main() {
-   
+	Checking cAcc("John Doe", 1000.0, 123456);
+
+	std::cout << "Amount: " << cAcc.GetBalance() << std::endl;
 }
