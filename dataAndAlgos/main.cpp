f7e86b90fa793655a77ad78c1edb9835c7807295
cal9t <cal9ten@gmail.com>
2025-07-21 18:47:03 -0400

    add in a reference for binary search

--- dataAndAlgos/main.cpp
+++ dataAndAlgos/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "recurSection.h"
+#include "binarySearchRef.h"
 
 const void printVector(const std::vector<int>& vec) {
 	for (const auto& elem : vec) {
@@ -10,6 +11,13 @@ const void printVector(const std::vector<int>& vec) {
 }
 
 int main() {
-
+	std::vector<int> test{ 1, 2, 3, 5, 6, 7, 9, 11, 13, 12 };
+	int here = binarySearchExample(test, 5);
+	if (here == -1) {
+		std::cout << "Element not found." << std::endl;
+	}
+	else {
+		std::cout << "Element found at index: " << here << std::endl;
+	}
 	return 0;
 }
