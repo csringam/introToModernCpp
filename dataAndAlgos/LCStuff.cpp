c79e897c8a13954d830939d78d6453aa72144021
cal9t <cal9ten@gmail.com>
2025-08-03 22:03:09 -0400

    hash map window

---
+++ dataAndAlgos/LCStuff.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+// Find largest window with 2 distinct fruits
+// Use a hasmap to act as window, which should contain 2 elements with thier frequency, and if there's a third
+// move the window start forward until there's only 2 elements again
+// i and j are the window indicies
+int totalFruit(vector<int>& fruits) {
+    unordered_map<int, int> window;
+    int i{ 0 }, j{ 0 }, out{ 0 };
+    while (j < fruits.size()) {
+        window[fruits[j]]++;
+        while (window.size() > 2) {
+            window[fruits[i]]--;
+            if (window[fruits[i]] == 0) window.erase(fruits[i]);
+            i++;
+        }
+        out = max(out, j - i + 1);
+        j++;
+    }
+    return out;
+}
